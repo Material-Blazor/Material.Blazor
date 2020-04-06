b1a438957f89481e2d1c699bc6f4aff3f866f756
MarkStega <mark2002@stega.us>
2020-04-06 08:38:13 -0400

    2020-04-06 WrappedInputComponent sample (broken)

---
+++ BlazorMdc.Demo.CommonUI/Pages/WrappedInputComponentDTO.cs
@@ -0,0 +1,14 @@
+﻿//
+//  2020-03-27  Mark Stega
+//              Created
+//
+
+using System.ComponentModel.DataAnnotations;
+namespace BlazorMdc.Demo.CommonUI.Pages
+{
+    public class LogonDTO
+    {
+        [Required(ErrorMessage = "PIN is required")] public string pPIN1 { get; set; }
+        [Required(ErrorMessage = "Validation PIN is required")] public string pPIN2 { get; set; }
+    }
+}
