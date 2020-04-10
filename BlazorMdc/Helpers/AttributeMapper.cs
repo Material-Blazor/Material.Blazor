3ece2c6b8a01ca403f9a6a35490eae690732e48d
Simon Ziegler <simon@dioptra.tech>
2020-04-10 18:31:04 +0100

    2020-04-10 Initial commit with compilation errors

---
+++ BlazorMdc/Helpers/AttributeMapper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorMdc
+{
+    public class AttributeMapper
+    {
+        public List<Func<KeyValuePair<string, object>>> Items = new List<Func<KeyValuePair<string, object>>>();
+    }
+
+
+    public static class AttributeMapperExtensions
+    {
+        public static T Add<T>(this T m, KeyValuePair<string, object> attribute) where T : AttributeMapper
+        {
+            m.Items.Add(() => attribute);
+            return m;
+        }
+
+
+        public static T Get<T>(this T m, Func<KeyValuePair<string, object>> funcName) where T : AttributeMapper
+        {
+            m.Items.Add(funcName);
+            return m;
+        }
+
+        public static T GetIf<T>(this T m, Func<KeyValuePair<string, object>?> funcName, Func<bool> func) where T : AttributeMapper
+        {
+            m.Items.Add(() => func() ? funcName() : null);
+            return m;
+        }
+
+        public static T If<T>(this T m, KeyValuePair<string, object> attribute, Func<bool> func) where T : AttributeMapper
+        {
+            m.Items.Add(() => func() ? attribute : null);
+            return m;
+        }
+    }
+}
