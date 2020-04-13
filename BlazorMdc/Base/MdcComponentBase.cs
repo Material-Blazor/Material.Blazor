f4ffe0f6a513576c41eb72207ff92bd8b8fc77d8
MarkStega <mark2002@stega.us>
2020-04-13 09:43:33 -0400

    2020-04-13 Commit to be able to examine PR

--- BlazorMdc/Base/MdcComponentBase.cs
+++ BlazorMdc/Base/MdcComponentBase.cs
@@ -2,7 +2,8 @@
 //  2020-03-31  Mark Stega
 //              Added ClassMapper/Class & StyleMapper/Style
 //
-
+//  2020-04-13  Mark Stega
+//              Reworked 
 using Microsoft.AspNetCore.Components;
 
 using System.Threading.Tasks;
