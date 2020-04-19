5341581af52282f761f07ca1b10a9c5ae14be8ff
Simon Ziegler <simon@dioptra.tech>
2020-04-19 12:54:10 +0100

    2020-04-19 Dialog bug fixes & Date Picker improvement (#38)
    * 2020-04-19 Dialog & Date Picker improvement
    
    Checkboxes and Radio Buttons render correctly in dialogs and Date Picker layout corrected back to Material Theme spec
    
    * 2020-04-19 Dialog & Date Picker improvement
    
    Checkboxes and Radio Buttons render correctly in dialogs and Date Picker layout corrected back to Material Theme spec. Nuget package number updated to 0.16.0

---
+++ BlazorMdc/Base/IMdcDialog.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorMdc
+{
+    public interface IMdcDialog
+    {
+        void RegisterLayoutAction(IMdcDialogChild child);
+    }
+}
