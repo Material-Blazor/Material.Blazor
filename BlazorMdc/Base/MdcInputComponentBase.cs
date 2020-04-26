eae83b423834efc1f27c0b4f5d7ea23ac01e3bdd
Simon Ziegler <simon@dioptra.tech>
2020-04-26 17:54:18 +0100

    2020-04-26 EoD
    Radio buttons in dialogs are broken, as are selects and autocompletes. Every component needs testing.

--- BlazorMdc/Base/MdcInputComponentBase.cs
+++ BlazorMdc/Base/MdcInputComponentBase.cs
@@ -13,16 +13,20 @@ namespace BlazorMdc
     // This is like InputBase from Microsoft.AspNetCore.Components.Forms, except that it treats
     // [CascadingParameter] EditContext as optional.
 
-    public abstract class MdcInputComponentBase<T> : MdcComponentBase
+    public abstract class MdcInputComponentBase<T> : MdcComponentBase, IMdcDialogChild
     {
         private bool _previousParsingAttemptFailed;
         private ValidationMessageStore _parsingValidationMessages;
         private Type _nullableUnderlyingType;
         private bool _hasSetInitialParameters;
         private T _value;
-        private bool hasRendered = false;
+        private bool _hasRendered = false;
+        private bool _instantiate = false;
 
         [CascadingParameter] EditContext CascadedEditContext { get; set; }
+
+        [CascadingParameter] private MdcDialog Dialog { get; set; }
+
 
         /// <summary>
         /// Gets or sets a collection of additional attributes that will be applied to the created element.
@@ -48,7 +52,7 @@ namespace BlazorMdc
                 var hasChanged = !EqualityComparer<T>.Default.Equals(value, _value);
                 if (hasChanged)
                 {
-                    if (hasRendered)
+                    if (_hasRendered)
                     {
                         Task.Run(async () => await ValueSetterAsync(value));
                     }
@@ -95,9 +99,11 @@ namespace BlazorMdc
         protected FieldIdentifier FieldIdentifier { get; private set; }
 
         /// <summary>
-        /// Gets or sets the current value of the input.
+        /// Gets or sets the value of the input. To be used by Mdc and PMdc components for binding to native components, or to set the value
+        /// in response to an event arising from the native component. In contrast the <see cref="Value"/> parameter is for use by BlazorMdc's consumers.
+        /// Do not mix these usages up.
         /// </summary>
-        protected T CurrentValue
+        protected T NativeComponentBoundValue
         {
             get => _value;
             set
@@ -115,9 +121,9 @@ namespace BlazorMdc
         /// <summary>
         /// Gets or sets the current value of the input, represented as a string.
         /// </summary>
-        protected string CurrentValueAsString
+        protected string NativeComponentBoundValueAsString
         {
-            get => FormatValueToString(CurrentValue);
+            get => FormatValueToString(NativeComponentBoundValue);
             set
             {
                 _parsingValidationMessages?.Clear();
@@ -130,12 +136,12 @@ namespace BlazorMdc
                     // Then all subclasses get nullable support almost automatically (they just have to
                     // not reject Nullable<T> based on the type itself).
                     parsingFailed = false;
-                    CurrentValue = default;
+                    NativeComponentBoundValue = default;
                 }
                 else if (TryParseValueFromString(value, out var parsedValue, out var validationErrorMessage))
                 {
                     parsingFailed = false;
-                    CurrentValue = parsedValue;
+                    NativeComponentBoundValue = parsedValue;
                 }
                 else
                 {
@@ -150,7 +156,7 @@ namespace BlazorMdc
 
                         _parsingValidationMessages.Add(FieldIdentifier, validationErrorMessage);
 
-                        // Since we're not writing to CurrentValue, we'll need to notify about modification from here
+                        // Since we're not writing to NativeComponentBoundValue, we'll need to notify about modification from here
                         EditContext.NotifyFieldChanged(FieldIdentifier);
                     }
                 }
@@ -165,7 +171,7 @@ namespace BlazorMdc
         }
 
         /// <summary>
-        /// Formats the value as a string. Derived classes can override this to determine the formating used for <see cref="CurrentValueAsString"/>.
+        /// Formats the value as a string. Derived classes can override this to determine the formating used for <see cref="NativeComponentBoundValueAsString"/>.
         /// </summary>
         /// <param name="value">The value to format.</param>
         /// <returns>A string representation of the value.</returns>
@@ -174,14 +180,14 @@ namespace BlazorMdc
 
         /// <summary>
         /// Parses a string to create an instance of <typeparamref name="T"/>. Derived classes can override this to change how
-        /// <see cref="CurrentValueAsString"/> interprets incoming values.
+        /// <see cref="NativeComponentBoundValueAsString"/> interprets incoming values.
         /// </summary>
         /// <param name="value">The string value to be parsed.</param>
         /// <param name="result">An instance of <typeparamref name="T"/>.</param>
         /// <param name="validationErrorMessage">If the value could not be parsed, provides a validation error message.</param>
         /// <returns>True if the value could be parsed; otherwise false.</returns>
         protected virtual bool TryParseValueFromString(string value, out T result, out string validationErrorMessage)
-            => throw new NotImplementedException($"This component does not parse string inputs. Bind to the '{nameof(CurrentValue)}' property, not '{nameof(CurrentValueAsString)}'.");
+            => throw new NotImplementedException($"This component does not parse string inputs. Bind to the '{nameof(NativeComponentBoundValue)}' property, not '{nameof(NativeComponentBoundValueAsString)}'.");
 
         /// <summary>
         /// Gets a string that indicates the status of the field being edited. This will include
@@ -189,6 +195,7 @@ namespace BlazorMdc
         /// </summary>
         protected string FieldClass
             => EditContext?.FieldCssClass(FieldIdentifier) ?? string.Empty;
+
 
         /// <inheritdoc />
         public override async Task SetParametersAsync(ParameterView parameters)
@@ -208,6 +215,11 @@ namespace BlazorMdc
                 EditContext = CascadedEditContext;
                 _nullableUnderlyingType = Nullable.GetUnderlyingType(typeof(T));
                 _hasSetInitialParameters = true;
+
+                if (Dialog != null)
+                {
+                    Dialog.RegisterLayoutAction(this);
+                }
             }
             else if (CascadedEditContext != EditContext)
             {
@@ -224,20 +236,31 @@ namespace BlazorMdc
             await base.SetParametersAsync(ParameterView.Empty);
         }
 
+
+        public void RequestInstantiation()
+        {
+            _instantiate = true;
+        }
+
+
+        protected override bool ShouldRender()
+        {
+            return _instantiate;
+        }
+
+
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
-            //if ((firstRender && (Dialog is null)) || instantiate)
-            //{
-            //    instantiate = false;
-            //    await jsRuntime.InvokeAsync<object>("BlazorMdc.radioButton.init", radioButtonReference, formReference, Checked);
-            //}
+            if ((firstRender && (Dialog is null)) || _instantiate)
+            {
+                _instantiate = false;
+                await InitializeMdcComponent();
+            }
 
             if (firstRender)
             {
-                hasRendered = true;
+                _hasRendered = true;
             }
-
-            await base.OnAfterRenderAsync(firstRender);
         }
     }
 }
