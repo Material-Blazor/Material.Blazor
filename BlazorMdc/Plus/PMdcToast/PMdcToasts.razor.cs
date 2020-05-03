39da5d4e51fcbeb1916010c606c69d5aadd33c67
MarkStega <mark2002@stega.us>
2020-05-03 08:01:32 -0400

    2020-05-03 Toast update (#64)

--- BlazorMdc/Plus/PMdcToast/PMdcToasts.razor.cs
+++ BlazorMdc/Plus/PMdcToast/PMdcToasts.razor.cs
@@ -12,23 +12,21 @@ using System.Timers;
 
 namespace BlazorMdc
 {
+    public enum IconType { FontAwesome, Material };
+
     public partial class PMdcToasts
     {
         [Inject] private IToastService ToastService { get; set; }
 
+        [Parameter] public IconType? IconType { get; set; }
         [Parameter] public string InfoClass { get; set; }
-        [Parameter] public string InfoIconClass { get; set; }
-        [Parameter] public string InfoIconName { get; set; }
+        [Parameter] public string InfoIcon { get; set; }
         [Parameter] public string SuccessClass { get; set; }
-        [Parameter] public string SuccessIconClass { get; set; }
-        [Parameter] public string SuccessIconName { get; set; }
+        [Parameter] public string SuccessIcon { get; set; }
         [Parameter] public string WarningClass { get; set; }
-        [Parameter] public string WarningIconClass { get; set; }
-        [Parameter] public string WarningIconName { get; set; }
+        [Parameter] public string WarningIcon { get; set; }
         [Parameter] public string ErrorClass { get; set; }
-        [Parameter] public string ErrorIconClass { get; set; }
-        [Parameter] public string ErrorIconName { get; set; }
-        [Parameter] public string IconClass { get; set; }
+        [Parameter] public string ErrorIcon { get; set; }
         [Parameter] public ToastPosition Position { get; set; } = ToastPosition.TopRight;
         [Parameter] public int Timeout { get; set; } = 5;
 
@@ -40,6 +38,15 @@ namespace BlazorMdc
             ToastService.OnShow += ShowToast;
 
             PositionClass = $"position-{Position.ToString().ToLower()}";
+
+            if ((!string.IsNullOrEmpty(InfoIcon)
+                 || !string.IsNullOrEmpty(SuccessIcon)
+                 || !string.IsNullOrEmpty(WarningIcon)
+                 || !string.IsNullOrEmpty(ErrorIcon)
+                ) && IconType == null)
+            {
+                throw new ArgumentException("If an icon is specified then IconType is a required parameter.");
+            }
         }
 
         public void RemoveToast(Guid toastId)
@@ -52,35 +59,27 @@ namespace BlazorMdc
             });
         }
 
-        private ToastSettings BuildToastSettings(ToastLevel level, string message, string heading)
+        private ToastSettings BuildToastSettings(ToastLevel level, RenderFragment message, string heading)
         {
-            if (!string.IsNullOrWhiteSpace(IconClass))
-            {
-                ErrorIconClass = IconClass;
-                InfoIconClass = IconClass;
-                SuccessIconClass = IconClass;
-                WarningIconClass = IconClass;
-            }
-
             switch (level)
             {
                 case ToastLevel.Error:
-                    return new ToastSettings(string.IsNullOrWhiteSpace(heading) ? "Error" : heading, message, "blazored-toast-error", ErrorClass, ErrorIconClass, ErrorIconName);
+                    return new ToastSettings(string.IsNullOrWhiteSpace(heading) ? "Error" : heading, message, IconType, "blazored-toast-error", ErrorClass, ErrorIcon);
 
                 case ToastLevel.Info:
-                    return new ToastSettings(string.IsNullOrWhiteSpace(heading) ? "Info" : heading, message, "blazored-toast-info", InfoClass, InfoIconClass, InfoIconName);
+                    return new ToastSettings(string.IsNullOrWhiteSpace(heading) ? "Info" : heading, message, IconType, "blazored-toast-info", InfoClass, InfoIcon);
 
                 case ToastLevel.Success:
-                    return new ToastSettings(string.IsNullOrWhiteSpace(heading) ? "Success" : heading, message, "blazored-toast-success", SuccessClass, SuccessIconClass, SuccessIconName);
+                    return new ToastSettings(string.IsNullOrWhiteSpace(heading) ? "Success" : heading, message, IconType, "blazored-toast-success", SuccessClass, SuccessIcon);
 
                 case ToastLevel.Warning:
-                    return new ToastSettings(string.IsNullOrWhiteSpace(heading) ? "Warning" : heading, message, "blazored-toast-warning", WarningClass, WarningIconClass, WarningIconName);
+                    return new ToastSettings(string.IsNullOrWhiteSpace(heading) ? "Warning" : heading, message, IconType, "blazored-toast-warning", WarningClass, WarningIcon);
             }
 
             throw new InvalidOperationException();
         }
 
-        private void ShowToast(ToastLevel level, string message, string heading)
+        private void ShowToast(ToastLevel level, RenderFragment message, string heading)
         {
             InvokeAsync(() =>
             {
