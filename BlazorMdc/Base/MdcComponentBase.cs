4c03865ba111e19b85e4eba5ec41837b79eeb802
Simon Ziegler <simon@dioptra.tech>
2020-05-15 14:51:32 +0100

    Intermediate commit

--- BlazorMdc/Base/MdcComponentBase.cs
+++ BlazorMdc/Base/MdcComponentBase.cs
@@ -6,12 +6,34 @@
 //              Reworked 
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using System;
 using System.Collections.Generic;
 using System.Security;
 using System.Threading.Tasks;
 
 namespace BlazorMdc
 {
+    /// <summary>
+    /// Determines what attributes to splat from <see cref="SplatAttributes"/>.
+    /// </summary>
+    internal enum SplatType
+    { 
+        /// <summary>
+        /// Return all attributes including values from <see cref="ClassMapper"/> and <see cref="StyleMapper"/>.
+        /// </summary>
+        AllIncludingMappers,
+
+        /// <summary>
+        /// Return only values from <see cref="ClassMapper"/> and <see cref="StyleMapper"/> - merges mappers with other class and style attributes.
+        /// </summary>
+        MappersOnly,
+
+        /// <summary>
+        /// Return all attributes except values from <see cref="ClassMapper"/> and <see cref="StyleMapper"/> - may include class and style.
+        /// </summary>
+        ExcludeMappers
+    }
+
     public abstract class MdcComponentBase : ComponentBase
     {
         [CascadingParameter] protected MdcCascadingDefaults CascadingDefaults { get; set; } = new MdcCascadingDefaults();
@@ -22,36 +44,61 @@ namespace BlazorMdc
         [Parameter(CaptureUnmatchedValues = true)] public IReadOnlyDictionary<string, object> UnmatchedAttributes { get; set; }
 
 
-        private Dictionary<string, object> _attributes;
-        internal Dictionary<string, object> Attributes
+        /// <summary>
+        /// Attributes That the component can elect to set for inclusion in SplatAttributes.
+        /// </summary>
+        internal IDictionary<string, object> ComponentSetAttributes { get; set; } = new Dictionary<string, object>(StringComparer.CurrentCultureIgnoreCase);
+
+
+        /// <summary>
+        /// Attributes ready for splatting in components. Guaranteed not null, unlike UnmatchedAttributes. Default parameter is <see cref="SplatType.AllIncludingMappers".
+        /// </summary>
+        internal IReadOnlyDictionary<string, object> SplatAttributes(SplatType splatType = SplatType.AllIncludingMappers)
         {
-            get
+            var result = new Dictionary<string, object>(ComponentSetAttributes);
+
+            if (UnmatchedAttributes != null && splatType != SplatType.MappersOnly)
             {
-                if (_attributes == null)
+                foreach (var item in UnmatchedAttributes)
                 {
-                    if (UnmatchedAttributes == null)
-                    {
-                        _attributes = new Dictionary<string, object>();
-                    }
-                    else
-                    {
-                        _attributes = new Dictionary<string, object>(UnmatchedAttributes);
-                    }
+                    result.Add(item.Key.ToLower(), item.Value);
+                }
+            }
+
+            if (splatType != SplatType.ExcludeMappers)
+            {
+                if (result.ContainsKey("class"))
+                {
+                    result["class"] += " " + ClassMapper.ToString();
+                }
+                else
+                {
+                    result.Add("class", ClassMapper.ToString());
                 }
 
-                return _attributes;
-
+                if (result.ContainsKey("style"))
+                {
+                    result["style"] += " " + StyleMapper.ToString();
+                }
+                else
+                {
+                    result.Add("style", StyleMapper.ToString());
+                }
             }
+
+            return result;
         }
 
-        internal bool IsDisabled => Attributes.ContainsKey("disabled");
+
+        /// <summary>
+        /// Determines whether the "disabled" attribute has been set.
+        /// </summary>
+        internal bool IsDisabled => UnmatchedAttributes != null && UnmatchedAttributes.ContainsKey("disabled") && Convert.ToBoolean(UnmatchedAttributes["disabled"]);
 
 
         protected ClassMapper ClassMapper { get; } = new ClassMapper();
 
         protected StyleMapper StyleMapper { get; } = new StyleMapper();
-
-        internal AttributeMapper AttributeMapper { get; } = new AttributeMapper();
 
 
 
@@ -68,24 +115,24 @@ namespace BlazorMdc
         }
 
         
-        protected MdcComponentBase()
-        {
-            ClassMapper.Get(() => Class);
+        //protected MdcComponentBase()
+        //{
+        //    ClassMapper.Get(() => Class);
 
-            StyleMapper.Get(() => Style);
-        }
+        //    StyleMapper.Get(() => Style);
+        //}
 
-        /// <summary>
-        /// Specifies one or more classnames for an DOM element.
-        /// </summary>
-        [Parameter]
-        public string Class { get; set; }
+        ///// <summary>
+        ///// Specifies one or more classnames for an DOM element.
+        ///// </summary>
+        //[Parameter]
+        //public string Class { get; set; }
 
 
-        /// <summary>
-        /// Specifies an inline style for an DOM element.
-        /// </summary>
-        [Parameter]
-        public string Style { get; set; }
+        ///// <summary>
+        ///// Specifies an inline style for an DOM element.
+        ///// </summary>
+        //[Parameter]
+        //public string Style { get; set; }
     }
 }
--- BlazorMdc/Helpers/AttributeBuilderRule.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlazorMdc
-{
-    public abstract class AttributeBuilderRule<T>
-    {
-        public abstract string GetName(T data);
-
-        public abstract object GetValue(T data);
-    }
-}
