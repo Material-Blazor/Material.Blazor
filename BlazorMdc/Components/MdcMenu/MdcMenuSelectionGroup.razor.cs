54fce4a628bb78b43d52998374a6fff7b72b2699
Simon Ziegler <simon@dioptra.tech>
2020-05-18 19:28:53 +0100

    Menu documented

---
+++ BlazorMdc/Components/MdcMenu/MdcMenuSelectionGroup.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorMdc
+{
+    /// <summary>
+    /// A Material Theme menu selection group.
+    /// </summary>
+    public partial class MdcMenuSelectionGroup
+    {
+        [Parameter] public RenderFragment ChildContent { get; set; }
+    }
+}
