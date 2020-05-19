66dbaaac90e636678af3f3517c6b6f3d5dba8a2f
Simon Ziegler <simon@dioptra.tech>
2020-05-19 16:48:45 +0100

    Numeric input, debounced input and shield docs

---
+++ BlazorMdc/Plus/PMdcDebouncedTextField/PMdcDebouncedTextField.razor.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Threading;
+
+namespace BlazorMdc
+{
+    /// <summary>
+    /// A Material Theme debounced text field.
+    /// </summary>
+    public partial class PMdcDebouncedTextField : MdcInputComponentBase<string>, IDisposable
+    {
+#nullable enable annotations
+        /// <summary>
+        /// The text input style.
+        /// </summary>
+        [Parameter] public MdcTextInputStyle? TextInputStyle { get; set; }
+
+
+        /// <summary>
+        /// The text alignment style.
+        /// </summary>
+        [Parameter] public MdcTextAlignStyle? TextAlignStyle { get; set; }
+
+
+        /// <summary>
+        /// Field label.
+        /// </summary>
+        [Parameter] public string Label { get; set; } = "";
+
+
+        /// <summary>
+        /// Hides the label if True. Defaults to False.
+        /// </summary>
+        [Parameter] public bool NoLabel { get; set; } = false;
+
+
+        /// <summary>
+        /// The leading icon's name. No leading icon shown if not set.
+        /// </summary>
+        [Parameter] public string? LeadingIcon { get; set; }
+
+
+        /// <summary>
+        /// The trailing icon's name. No leading icon shown if not set.
+        /// </summary>
+        [Parameter] public string? TrailingIcon { get; set; }
+
+
+        /// <summary>
+        /// The foundry to use for both leading and trailing icons.
+        /// <para><c>IconFoundry="MdcIconHelper.MIIcon()"</c></para>
+        /// <para><c>IconFoundry="MdcIconHelper.FAIcon()"</c></para>
+        /// <para><c>IconFoundry="MdcIconHelper.OIIcon()"</c></para>
+        /// </summary>
+        [Parameter] public IMdcIconFoundry? IconFoundry { get; set; }
+
+
+        /// <summary>
+        /// Debounce interval in milliseconds.
+        /// </summary>
+        [Parameter] public int? DebounceInterval { get; set; }
+#nullable restore annotations
+
+
+        private Timer Timer { get; set; }
+        private string CurrentValue { get; set; } = "";
+        private int AppliedDebounceInterval => CascadingDefaults.AppliedDebounceInterval(DebounceInterval);
+
+
+        /// <inheritdoc/>
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            CurrentValue = Value;
+        }
+
+
+        /// <inheritdoc/>
+        public void Dispose() => Timer?.Dispose();
+
+
+        private void OnTextInput(ChangeEventArgs eventArgs)
+        {
+            Timer?.Dispose();
+            CurrentValue = eventArgs.Value.ToString();
+            var autoReset = new AutoResetEvent(false);
+            Timer = new Timer(OnTimerComplete, autoReset, AppliedDebounceInterval, Timeout.Infinite);
+        }
+
+
+        private void OnTimerComplete(object stateInfo) => InvokeAsync(() => ReportingValue = CurrentValue);
+    }
+}
---
+++ BlazorMdc/Plus/PMdcDivider/PMdcDivider.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace BlazorMdc
+{
+    /// <summary>
+    /// A Material Theme divider.
+    /// </summary>
+    public partial class PMdcDivider : MdcComponentBase
+    {
+        /// <summary>
+        /// Material Theme "mdc-list-divider--inset" if True.
+        /// </summary>
+        [Parameter] public bool Inset { get; set; }
+
+
+        /// <summary>
+        /// Material Theme "mdc-list-divider--padded" if True.
+        /// </summary>
+        [Parameter] public bool Padded { get; set; }
+
+
+        /// <inheritdoc/>
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            ClassMapper
+                .Add("mdc-list-divider")
+                .AddIf("mdc-list-divider--inset", () => Inset)
+                .AddIf("mdc-list-divider--padded", () => Padded);
+        }
+
+    }
+}
