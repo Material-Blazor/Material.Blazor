2aa76524880b79afeccbbde0c7d176cc5e544a5d
Simon Ziegler <simon@dioptra.tech>
2020-07-24 08:43:46 +0100

    Intermediate

--- BlazorMdc.NodeMaterialComponents/webpack.mix.js
+++ BlazorMdc.NodeMaterialComponents/webpack.mix.js
@@ -40,7 +40,7 @@ mix
                                     includePaths: ['node_modules'],
                                 },
                             },
-                        },
+                        }
                     ]
                 }
             ]
