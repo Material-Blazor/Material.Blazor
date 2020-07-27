6827c3075d0112e520269674cbd8311bf110f194
Mark Stega <mark2002@stega.us>
2020-07-27 14:10:31 -0400

    Remove wip/wwwroot working files

--- BlazorMdc.MaterialComponents/Build-wwwroot.cmd
+++ BlazorMdc.MaterialComponents/Build-wwwroot.cmd
@@ -1,18 +1,16 @@
 @echo on
 call npm install
+call npm run clean-wip
 call npm run clean-wwwroot
-call npm run build-scss-matfont
-call npm run minify-css-matfont
-call npm run delete-css-matfont
-call npm run build-scss-bundle
-call npm run minify-css-bundle
-call npm run delete-css-bundle
-call npm run build-scss-bmdc
-call npm run minify-css-bmdc
-call npm run copy-material-css
-call npm run copy-material-js
-@echo call npm run build-ts
-@echo call npm run minify-js
-@echo call npm run copy-all
+call npm run build-matfont
+call npm run minify-matfont
+call npm run build-bmdc
+call npm run minify-bmdc
+call npm run build-bundle
+call npm run minify-bundle-css
+call npm run minify-bundle-js
+call npm run copy-all
+dir wip /s
 dir wwwroot /s
+@echo call npm run clean-wip
 @echo call npm run clean-wwwroot
