9f4a3c5e59a5883f23f5846dbccc08034a8d22f8
Mark Stega <mark2002@stega.us>
2020-07-28 09:20:30 -0400

    2020-07-28 Feature complete; Need task runner

--- BlazorMdc/Build-wwwroot.cmd
+++ BlazorMdc/Build-wwwroot.cmd
@@ -9,10 +9,9 @@ call npm run minify-bmdc
 call npm run build-bundle
 call npm run minify-bundle-css
 call npm run build-bmdc-ts
+call npm run minify-bmdc-js
 call npm run minify-bundle-js
-call npm run copy-all
 call npm run build-bmdcdemo
 dir wip /s
 dir wwwroot /s
 call npm run clean-wip
-call npm run clean-wwwroot
--- ConvertUTF16toUTF8.ps1
+++
@@ -1,4 +0,0 @@
-Get-ChildItem -Recurse *.scss | foreach {
-$FileName = $_.FullName;
-[System.Io.File]::ReadAllText($FileName) | Out-File -FilePath $FileName -Encoding UTF8;
-}
