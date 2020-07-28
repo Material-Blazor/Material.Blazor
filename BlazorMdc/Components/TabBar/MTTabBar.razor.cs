aad57fea8744fd8904f92f3d2e73de1539f52f46
Simon Ziegler <simon@dioptra.tech>
2020-07-28 13:17:28 +0100

    x

--- BlazorMdc/Components/TabBar/MTTabBar.razor.cs
+++ BlazorMdc/Components/TabBar/MTTabBar.razor.cs
@@ -73,7 +73,7 @@ namespace BlazorMdc
             ClassMapper
                 .Add("mdc-tab-bar")
                 .AddIf(DensityInfo.CssClassName, () => DensityInfo.ApplyCssClass);
-            ForceShouldRenderToTrue = true;
+            
             OnValueSet += OnValueSetCallback;
         }
 
