f50a94bd74bf04bc6860838a1dd1278067245768
Simon Ziegler <simon@dioptra.tech>
2020-08-19 10:37:23 +0100

    SameSite cookie config

--- BlazorMdc/Model/ServiceCollectionExtensions.cs
+++ BlazorMdc/Model/ServiceCollectionExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using BlazorMdc.Internal;
-
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.DependencyInjection;
-using System.Runtime.CompilerServices;
 
 namespace BlazorMdc
 {
