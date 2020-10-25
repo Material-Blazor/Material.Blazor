e6c51fe87018626b1c619bc7267c18d162e4ae44
Simon Ziegler <simon@dioptra.tech>
2020-10-25 12:54:02 +0000

    Update cascading values with IsFixed="true" (#310)
    * Cascading values optimized per Steve Sanderson's suggestion
    
    See https://github.com/dotnet/aspnetcore/issues/26230
    
    * Further change

--- Material.Blazor/Foundation/InputComponentFoundation.cs
+++ Material.Blazor/Foundation/InputComponentFoundation.cs
@@ -101,13 +101,13 @@ namespace Material.Blazor.Internal
             get => _componentValue;
             set
             {
-#if Logging
-                Logger.LogDebug("ComponentValue setter entered");
+#if LoggingVerbose
+                Logger.LogDebug($"ComponentValue setter entered: _componentValue is '{_cachedValue?.ToString() ?? "null"}' and new value is'{value?.ToString() ?? "null"}'");
 #endif
                 if (!EqualityComparer<T>.Default.Equals(value, _componentValue))
                 {
 #if Logging
-                    Logger.LogDebug("ComponentValue setter changing value to '" + value?.ToString() ?? "null" + "'");
+                    Logger.LogDebug($"ComponentValue setter changed _componentValue");
 #endif
                     _componentValue = value;
                     _ = ValueChanged.InvokeAsync(value);
@@ -280,7 +280,7 @@ namespace Material.Blazor.Internal
                 _nullableUnderlyingType = Nullable.GetUnderlyingType(typeof(T));
                 _hasSetInitialParameters = true;
 #if Logging
-                Logger.LogDebug("SetParametersAsync setting ComponentValue value to '" + Value?.ToString() ?? "null" + "'");
+                Logger.LogDebug($"SetParametersAsync setting ComponentValue value to '{Value?.ToString() ?? "null"}'");
 #endif
                 _cachedValue = Value;
                 _componentValue = Value;
@@ -292,8 +292,7 @@ namespace Material.Blazor.Internal
                 // We don't support changing EditContext because it's messy to be clearing up state and event
                 // handlers for the previous one, and there's no strong use case. If a strong use case
                 // emerges, we can consider changing this.
-                throw new InvalidOperationException($"{GetType()} does not support changing the " +
-                    $"{nameof(EditContext)} dynamically.");
+                throw new InvalidOperationException($"{GetType()} does not support changing the {nameof(EditContext)} dynamically.");
             }
 
             // For derived components, retain the usual lifecycle with OnInit/OnParametersSet/etc.
@@ -318,16 +317,19 @@ namespace Material.Blazor.Internal
 
         private void CommonParametersSet()
         {
+#if LoggingVerbose
+            Logger.LogDebug($"OnParametersSet setter entered: _cachedValue is '{_cachedValue?.ToString() ?? "null"}' and Value is'{Value?.ToString() ?? "null"}'");
+#endif
             if (!EqualityComparer<T>.Default.Equals(_cachedValue, Value))
             {
                 _cachedValue = Value;
 #if Logging
-                Logger.LogDebug("OnParametersSet setting CachedValue value to '" + Value?.ToString() ?? "null" + "'");
+                Logger.LogDebug($"OnParametersSet changed _cachedValue value");
 #endif
-                if (!EqualityComparer<T>.Default.Equals(ComponentValue, Value))
+                if (!EqualityComparer<T>.Default.Equals(_componentValue, Value))
                 {
 #if Logging
-                    Logger.LogDebug("OnParametersSet setting ComponentValue value to '" + Value?.ToString() ?? "null" + "'");
+                    Logger.LogDebug("OnParametersSet update _componentValue value from '" + _componentValue?.ToString() ?? "null" + "'");
 #endif
                     _componentValue = Value;
                     if (_hasInstantiated)
