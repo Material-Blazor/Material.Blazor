24efc89fb9c491d71c19b8a5d6893e6b28e8c707
Stefan Lörwald <stefan.loerwald@gmail.com>
2021-02-23 09:00:01 +0100

    merge init&open for snackbar, as they are done in immediate succession anyway

--- Material.Blazor/Components/Snackbar/InternalSnackbar.razor.cs
+++ Material.Blazor/Components/Snackbar/InternalSnackbar.razor.cs
@@ -55,21 +55,12 @@ namespace Material.Blazor.Internal
 
 
         /// <inheritdoc/>
-        private protected override async Task InstantiateMcwComponent() => await JsRuntime.InvokeVoidAsync("MaterialBlazor.MBSnackbar.init", SnackbarReference, ObjectReference);
+        private protected override async Task InstantiateMcwComponent() => await JsRuntime.InvokeVoidAsync("MaterialBlazor.MBSnackbar.init", SnackbarReference, ObjectReference, Snackbar.Settings.AppliedTimeout);
         
         
         /// <inheritdoc/>
         private protected override async Task DestroyMcwComponent() => await JsRuntime.InvokeVoidAsync("MaterialBlazor.MBSnackbar.destroy", SnackbarReference);
         
-        
-        // Would like to use <inheritdoc/> however DocFX cannot resolve to references outside Material.Blazor
-        protected override async Task OnAfterRenderAsync(bool firstRender)
-        {
-            if (firstRender)
-            {
-                await JsRuntime.InvokeVoidAsync("MaterialBlazor.MBSnackbar.open", SnackbarReference, Snackbar.Settings.AppliedTimeout);
-            }
-            await base.OnAfterRenderAsync(firstRender);
-        }
+
     }
 }
