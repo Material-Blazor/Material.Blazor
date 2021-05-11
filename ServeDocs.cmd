6040dd1182b2186a1a35ca85f2b8ca84fc27d62d
Mark Stega <mark2002@stega.us>
2021-05-11 14:23:33 -0400

    Update to servedocs with preparation for docfx 3.0

--- ServeDocs.cmd
+++ ServeDocs.cmd
@@ -1,4 +1,7 @@
 @echo on
+rem dotnet tool install -g docfx --version "3.0.0-*" --add-source https://docfx.pkgs.visualstudio.com/docfx/_packaging/docs-public-packages/nuget/v3/index.json
+rem docfx build 
+rem docfx serve siteDocFx
 rmdir /s /q api
 rmdir /s /q siteDocFx
-.\DocFx\docfx.exe .\docfx.json --serve
+docfx\docfx.exe .\docfx.json --serve
