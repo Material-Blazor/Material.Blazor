cd591c42c20577ae9d783d8669732de1c069d4dc
tanekim77 <tanekim77@hotmail.com>
2021-05-30 09:55:23 -0400

    Exposes tooltip configurations

--- Material.Blazor/Components/Tooltip/MBTooltip.ts
+++ Material.Blazor/Components/Tooltip/MBTooltip.ts
@@ -1,4 +1,5 @@
 ﻿import { MDCTooltip } from '@material/tooltip';
+export  { numbers } from '@material/tooltip';
 
 export function init(arrayOfReferences) {
     for (const elem of arrayOfReferences) {
