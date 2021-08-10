6bd99d45de12e09f8bdeecee9c94980ee5943304
Mark Stega <mark2002@stega.us>
2021-08-10 08:40:56 -0400

    Remove NoThrow service injection from test project

--- Material.Blazor.Test/Mocking.cs
+++ Material.Blazor.Test/Mocking.cs
@@ -16,7 +16,6 @@ namespace Material.Blazor.Test
             ctx = new();
             _ = ctx.Services
                 .AddSingleton(new Mock<IBatchingJSRuntime>().Object)
-                .AddSingleton(new Mock<INoThrowDotNetObjectReferenceFactory>().Object)
                 .AddSingleton(new Mock<IMBLoggingService>().Object)
                 .AddSingleton(new Mock<IMBTooltipService>().Object)
                 .AddSingleton(new Mock<IMBToastService>().Object)
