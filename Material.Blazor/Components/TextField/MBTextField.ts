78912e3948b4ab798d6ac4b4dc3da662a8071e4c
Simon Ziegler <simon@dioptra.tech>
2021-10-13 15:39:52 +0100

    Further numeric field improvement

--- Material.Blazor/Components/TextField/MBTextField.ts
+++ Material.Blazor/Components/TextField/MBTextField.ts
@@ -68,13 +68,13 @@ export function setType(elem, value, inputElem, type, formNoValidate) {
 
     elem._textField.value = value;
 
-    // Commented out to eliminate race condition where if you (i) select a numeric field, (ii) click a different window
+    // Check focus to eliminate race condition where if you (i) select a numeric field, (ii) click a different window
     // then (iii) click back and straight into another numeric field, the two fields would conflict and flash between them.
     // Seemed to happen in Blazor Server (presumably due to slower JS Invoke timing). Also probably not needed. Time will tell.
-    //if (formNoValidate) {
-    //    inputElem.focus();
-    //    inputElem.select();
-    //}
+    if (formNoValidate && elem._textField.foundation.adapter.isFocused() == true) {
+        //inputElem.focus();
+        inputElem.select();
+    }
 }
 /*!
  * Sanitize and encode all HTML in a user-submitted string
