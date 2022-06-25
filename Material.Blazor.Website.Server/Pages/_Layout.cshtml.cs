2646398cba90a4b5e9426d10cb8f5ca701efcf97
Simon Ziegler <simon@dioptra.tech>
2022-06-25 13:38:05 +0100

    Blazor Server head outlets implemented plus page titles (#648)

---
+++ Material.Blazor.Website.Server/Pages/_Layout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Material.Blazor.Website.Server.Pages
+{
+    public class _LayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
