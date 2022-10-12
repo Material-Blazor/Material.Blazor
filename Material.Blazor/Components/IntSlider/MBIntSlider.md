e77f7443618d416da6e5f137f5fd51a131eba1da
Simon Ziegler <simon@dioptra.tech>
2022-10-12 14:10:40 +0100

    Add int slider (#723)

---
+++ Material.Blazor/Components/IntSlider/MBIntSlider.md
@@ -0,0 +1,28 @@
+﻿---
+uid: C.MBIntSlider
+title: MBIntSlider
+---
+# MBSlider
+
+## Summary
+
+A single thumb [Material Slider](https://github.com/material-components/material-components-web/tree/v9.0.0/packages/mdc-slider#slider) working on integers.
+
+## Details
+
+- Two-way binds an int Value parameter;
+- Implements only discrete slider variants.
+- Emits events on:
+  - Thumb-up;
+  - Debounced, meaning that events are emitted once the slider stops moving, but before thumb-up; or
+  - Throttled, so that events are emitted continuously as the slider is moved.
+- Debounce and throttle implemented in JS to avoid excessive/inefficient use of Blazor JSInterop.
+
+- ValueMin and ValueMax are set in OnInitialized and not in SetParameters
+
+&nbsp;
+
+&nbsp;
+
+[![Components](https://img.shields.io/static/v1?label=Components&message=Core&color=blue)](xref:A.CoreComponents)
+[![Docs](https://img.shields.io/static/v1?label=API%20Documentation&message=MBIntSlider&color=brightgreen)](xref:Material.Blazor.MBIntSlider)
---
+++ Material.Blazor/Components/IntSlider/MBIntSlider.razor.cs
@@ -0,0 +1,68 @@
+﻿using Material.Blazor.Internal;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace Material.Blazor;
+
+/// <summary>
+/// A Material Theme single-thumb slider.
+/// </summary>
+public partial class MBIntSlider : InputComponent<int>
+{
+    /// <summary>
+    /// Shows tickmarks if true.
+    /// </summary>
+    [Parameter] public bool ShowTickmarks { get; set; } = false;
+
+
+    /// <summary>
+    /// The minimum slider value.
+    /// </summary>
+    [Parameter] public int ValueMin { get; set; } = 0;
+
+
+    /// <summary>
+    /// The maximum slider value.
+    /// </summary>
+    [Parameter] public int ValueMax { get; set; } = 100;
+
+
+    /// <summary>
+    /// Specifies how slider events are emitted, see <see cref="MBInputEventType"/>.
+    /// </summary>
+    [Parameter] public MBInputEventType EventType { get; set; } = MBInputEventType.OnChange;
+
+
+    /// <summary>
+    /// For continuous input sets the debounce/throttle delay.
+    /// </summary>
+    [Parameter] public uint ContinuousInputDelay { get; set; } = 300;
+
+
+    /// <summary>
+    /// Value for the "aria-label" tag.
+    /// </summary>
+    [Parameter] public string AriaLabel { get; set; } = "Slider";
+
+
+    private decimal DecimalValue
+    {
+        get => ComponentValue;
+        set => ComponentValue = Convert.ToInt32(Math.Round(value, 0));
+    }
+
+
+    private int NumSteps => ValueMax - ValueMin;
+    private MBSliderType SliderType => ShowTickmarks ? MBSliderType.DiscreteWithTickmarks : MBSliderType.Discrete;
+
+
+    // Would like to use <inheritdoc/> however DocFX cannot resolve to references outside Material.Blazor
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+
+        ForceShouldRenderToTrue = true;
+    }
+}
