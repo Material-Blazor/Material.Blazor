ac7c23cad870ac67be7e83ae2333b8ca94f6ac94
Mark Stega <mark2002@stega.us>
2023-08-03 17:13:10 -0400

    2023-08-03 EoD

---
+++ Material.Blazor.MD3/Components/ProgressIndicator/MBProgressIndicator.cs
@@ -0,0 +1,73 @@
+﻿using Material.Blazor.Internal;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Material.Blazor
+{
+    public sealed class MBProgressIndicator : ComponentFoundation
+    {
+        #region members
+
+        [Parameter] public double DeterminantValue { get; set; } = 0.0f;
+        [Parameter] public bool IsDeterminant { get; set; } = false;
+        [Parameter] public bool IsLinear { get; set; } = false;
+        [Parameter] public bool IsFourColor { get; set; } = false;
+
+        #endregion
+
+        #region BuildRenderTree
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            var attributesToSplat = AttributesToSplat().ToArray();
+
+            if (IsLinear)
+            {
+                builder.OpenElement(0, "md-linear-progress");
+            }
+            else
+            {
+                builder.OpenElement(0, "md-circular-progress");
+            }
+
+            if (attributesToSplat.Any())
+            {
+                builder.AddMultipleAttributes(1, attributesToSplat);
+            }
+
+            if (AppliedDisabled)
+            {
+                builder.AddAttribute(2, "disabled");
+            }
+
+            builder.AddAttribute(3, "class", @class);
+            builder.AddAttribute(4, "style", style);
+            builder.AddAttribute(5, "id", id);
+
+            if (IsDeterminant)
+            {
+                builder.AddAttribute(6, "value", DeterminantValue.ToString());
+            }
+            else
+            {
+                builder.AddAttribute(6, "indeterminant");
+            }
+
+            if (IsFourColor)
+            {
+                builder.AddAttribute(7, "four-color");
+            }
+
+            builder.CloseElement();
+        }
+
+        #endregion
+
+    }
+}
---
+++ Material.Blazor.MD3/Components/ProgressIndicator/MBProgressIndicator.md
@@ -0,0 +1,24 @@
+﻿---
+uid: C.MBProgressIndicator
+title: MBProgressIndicator
+---
+# MBProgressIndicator
+
+## Summary
+
+A simple progress indicator that can be circular
+[Material Progress Circular](https://material-web.dev/components/circular-progress/)
+or linear
+[Material Progress Linear](https://material-web.dev/components/linear-progress/)
+with either indeterminant or determinant characteristics.
+Applies [density subsystem](xref:A.Density).
+
+## Reserved Attributes
+
+The following attributes are reserved by Material Components Web and will be ignored if you supply them:
+
+- role
+- type
+
+[![Components](https://img.shields.io/static/v1?label=Components&message=Core&color=blue)](xref:A.CoreComponents)
+[![Docs](https://img.shields.io/static/v1?label=API%20Documentation&message=MBProgressIndicator&color=brightgreen)](xref:Material.Blazor.MBProgressIndicator)
