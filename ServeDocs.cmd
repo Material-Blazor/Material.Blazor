6f0ba4be45645dd7d733aca2b452ee74d9845ec4
Mark Stega <mark2002@stega.us>
2023-08-15 09:53:24 -0400

    New WiP using long line support

--- ServeDocs.cmd
+++ ServeDocs.cmd
@@ -1,10 +1,13 @@
 @echo on
+if exist api rd api /s /q
 if exist SiteDocFx rd SiteDocFx /s /q
-copy Material.Blazor\docfxMD2.json docfx.json /Y
-copy Material.Blazor\indexMD2.md index.md /Y
-copy Material.Blazor\tocMD2.yml toc.yml /Y
+copy Material.Blazor\docfx.json.MD2 docfx.json /Y
+copy Material.Blazor\index.md.MD2 index.md /Y
+copy Material.Blazor\toc.yml.MD2 toc.yml /Y
+rem dotnet tool update -g docfx
 dotnet build Material.Blazor/Material.Blazor.csproj --configuration Server
 docfx docfx.json --serve
+if exist api rd api /s /q
 if exist SiteDocFx rd SiteDocFx /s /q
 del docfx.json
 del index.md
--- ServeDocsMD3.cmd
+++ ServeDocsMD3.cmd
@@ -1,10 +1,13 @@
 @echo on
+if exist api rd api /s /q
 if exist SiteDocFxMD3 rd SiteDocFxMD3 /s /q
-copy Material.Blazor.MD3\docfxMD3.json docfx.json
-copy Material.Blazor.MD3\indexMD3.md index.md
-copy Material.Blazor.MD3\tocMD3.yml toc.yml
+copy Material.Blazor.MD3\docfx.json.MD3 docfx.json
+copy Material.Blazor.MD3\index.md.MD3 index.md
+copy Material.Blazor.MD3\toc.yml.MD3 toc.yml
+rem dotnet tool update -g docfx
 dotnet build Material.Blazor.MD3/Material.Blazor.MD3.csproj --configuration Server
 docfx docfx.json --serve
+if exist api rd api /s /q
 if exist SiteDocFxMD3 rd SiteDocFxMD3 /s /q
 del docfx.json
 del index.md
