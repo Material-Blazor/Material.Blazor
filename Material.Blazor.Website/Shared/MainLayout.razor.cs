214766f08a73f9259bcf4ef578a7e35b2a28fd51
Mark Stega <mark2002@stega.us>
2023-08-17 11:08:25 -0400

    More documentation updates

---
+++ Material.Blazor.Website/Shared/MainLayout.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace Material.Blazor.Website.Shared
+{
+    public partial class MainLayout
+    {
+        private const string materialDocRef = "https://github.com/material-components/material-components-web/tree/master/packages/";
+        private MBDrawer Drawer { get; set; }
+        private MBMenu Menu { get; set; }
+        private string Theme { get; set; } = "material-default-theme";
+
+        private async Task OpenMenuAsync()
+        {
+            await Menu.ToggleAsync();
+        }
+
+        protected async Task ThemeSetterAsync(string theme)
+        {
+            await JsRuntime.InvokeAsync<object>("material_blazor_website.themeSetter.setTheme", theme, true);
+            Theme = theme;
+            StateHasChanged();
+        }
+
+
+        private void ListItemClickHandler(string NavigationReference)
+        {
+            Drawer.NotifyNavigation();
+            NavigationService.NavigateTo(NavigationReference);
+        }
+
+        private void SideBarToggle()
+        {
+            Drawer.Toggle();
+        }
+    }
+}
