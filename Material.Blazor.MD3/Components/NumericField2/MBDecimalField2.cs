618ae8a7ef7df1b83f4b8ef3bd9516abce06ae41
Mark Stega <mark2002@stega.us>
2023-08-29 10:31:07 -0400

    Demo of MBDecimalField2

---
+++ Material.Blazor.MD3/Components/NumericField2/MBDecimalField2.cs
@@ -0,0 +1,14 @@
+﻿using Material.Blazor.Internal;
+
+namespace Material.Blazor;
+
+/// <summary>
+/// A Material.Blazor filled style formatted decimal field.
+/// </summary>
+public sealed class MBDecimalField2 : InternalFloatingPointField2Base<decimal, MBTextField2>
+{
+    private protected override decimal ConvertFromDecimal(decimal decimalValue)
+    {
+        return decimalValue;
+    }
+}
