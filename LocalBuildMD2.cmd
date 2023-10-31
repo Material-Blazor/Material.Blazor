39db4beaadd0f7c8f2256a1d70822036feaa13cf
Mark Stega <mark2002@stega.us>
2023-10-31 12:42:59 -0400

    2023-10-31 MoD - Get new package-lock.json in place (M.B & M.B.MD3)

---
+++ LocalBuildMD2.cmd
@@ -0,0 +1,15 @@
+@echo off
+set argVersion=%1
+if "%1" == "" set argVersion=5.0.0-rc3
+set argDestination=%2
+if "%2" == "" set argDestination="c:\solutions\local nuget packages"
+
+echo Version is %argVersion%
+echo Destination is %argDestination%
+echo Beginning build
+echo ...
+dotnet pack Material.Blazor/Material.Blazor.csproj -p:Version=%argVersion% --configuration Server --output %argDestination% -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+echo ...
+echo Build results
+echo ...
+dir %argDestination%
