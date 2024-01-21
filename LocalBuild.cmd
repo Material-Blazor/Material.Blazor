62d29ed9949aae2232e806d70227785172b4e6fb
Mark Stega <mark2002@stega.us>
2024-01-21 08:05:55 -0500

    2024-01-21 BoD

--- LocalBuild.cmd
+++ LocalBuild.cmd
@@ -1,6 +1,6 @@
 @echo off
 set argVersion=%1
-if "%1" == "" set argVersion=5.0.6
+if "%1" == "" set argVersion=5.0.100
 set argDestination=%2
 if "%2" == "" set argDestination="c:\solutions\local nuget packages"
 
