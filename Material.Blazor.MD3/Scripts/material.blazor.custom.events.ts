74affb393173dd7d294ffc002c91e3f41ac03840
Mark Stega <mark2002@stega.us>
2024-02-01 16:52:14 -0500

    2024-02-01 EoD

--- Material.Blazor.MD3/Scripts/material.blazor.custom.events.ts
+++ Material.Blazor.MD3/Scripts/material.blazor.custom.events.ts
@@ -4,6 +4,9 @@
 ** 
 ** After being created the module code must be removed and 'export' must be added to the
 ** two function calls. The file is then copied to 'Material.Blazor.MD3.lib.module.js'.
+**
+** 'Material.Blazor.MD3.lib.module.js' is checked into the repository in order to insure
+** inclusion as a static asset of M.B.MD3.
 */
 
 import { CloseMenuEvent } from '@material/web/menu/internal/controllers/shared.js';
