2f36f1c053d5c794e78e2ea3695d2771f02ad4eb
Mark Stega <mark2002@stega.us>
2024-09-25 08:03:36 -0400

    npmupdate, DocFx addition to M.B.csproj

--- ServeDocs.cmd
+++ ServeDocs.cmd
@@ -1,6 +1,6 @@
 @echo on
 if exist ".artifact\api" rd ".artifacts\api" /s /q
 if exist ".artifacts\SiteDocFx" rd ".artifacts\SiteDocFx" /s /q
-rem dotnet tool update -g docfx
+dotnet tool update -g docfx
 dotnet build Material.Blazor/Material.Blazor.csproj --configuration Server
 docfx docfx.json --serve
